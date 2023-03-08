d3b33566f93223744fd3c12f8549cea831243014
Valentina Aldalla <aldallavalentina2@gmail.com>
2023-03-08 00:27:34 -0300

    back

---
+++ miporfolio/backend/va/src/test/java/com/portfolio/va/VaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.va;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
